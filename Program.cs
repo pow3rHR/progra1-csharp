f49408cf77b27a416d73202c6d44175079b6bc48
Diego Herrera <psystingod@gmail.com>
2023-07-25 22:17:41 -0600

    e7 was added

--- Program.cs
+++ Program.cs
@@ -1,15 +1,3 @@
-﻿// Ejercicio #6
+﻿// Ejercicio #7
 
-// decimal a binario
-Console.Write("Por favor ingrese un número decimal para ser convertido a binario: ");
-
-string n = Console.ReadLine();
-
-Console.WriteLine(Convert.ToString(Convert.ToInt32(n), 2));
-
-// binario a decimal
-Console.Write("Por favor ingrese un número binario para ser convertido a decimal: ");
-
-string b = Console.ReadLine();
-
-Console.WriteLine(Convert.ToInt32(b, 2));
+Console.WriteLine("\t\t\t*\t\t\t\n\t\t*\t\t*\n\t*\t\t\t\t*");
