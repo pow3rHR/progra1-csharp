bab61b8b3ef43428368527ad47f7901927767eac
Diego Herrera <psystingod@gmail.com>
2023-07-25 21:30:17 -0600

    ej1 was added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ejercicio #1
+
+int x = 5, y = 8;
+
+y = x;
+x = x * x;
+y = y++;
+
+double z = x - y;
+
+Console.WriteLine(z--);
