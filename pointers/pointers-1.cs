ec1ab9dc973b44524ace0eb0b04bc0b26ba13d50
Diego Herrera <psystingod@gmail.com>
2023-07-28 11:12:57 -0600

    pointers-1.cs was added

--- pointers/pointers-1.cs
+++ pointers/pointers-1.cs
@@ -4,4 +4,23 @@
 
 // c# ademas nos provee de contextos inseguros donde si podemos asignar, liberar y acceder a memoria.
 
-int* miPuntero;
+int[] list = new int[3] {1, 2, 3};
+
+int n = 30;
+
+string nombre = "Diego";
+
+unsafe {
+    fixed(int* miPuntero = &list[0]) {
+        Console.WriteLine(*miPuntero);
+    };
+
+    int* nPuntero = &n;
+    Console.WriteLine(*nPuntero);
+
+    //tambien podriamos mostrar letra por letra
+    fixed(char* nombrePuntero = nombre) {
+        Console.WriteLine(*nombrePuntero);
+    }
+
+}
