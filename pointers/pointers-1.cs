52f7c26c0c043437d066d64f5cedc7116bca3a51
Diego Herrera <psystingod@gmail.com>
2023-07-28 10:30:59 -0600

    pointers-1.cs was added

---
+++ pointers/pointers-1.cs
@@ -0,0 +1,7 @@
+// Los punteros son tipos de datos especiales que nos permiten acceder a valores que estan en
+// posiciones de memoria especificas. Por defecto c# provee herramientas seguras que no permiten
+// que accedamos directamente a memoria ni podamos asignar o liberar memoria.
+
+// c# ademas nos provee de contextos inseguros donde si podemos asignar, liberar y acceder a memoria.
+
+int* miPuntero;
