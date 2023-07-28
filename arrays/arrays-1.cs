35712039c11a0ee7cfb808b50db442deac307c91
Diego Herrera <psystingod@gmail.com>
2023-07-27 21:26:56 -0600

    arrays-1 added

---
+++ arrays/arrays-1.cs
@@ -0,0 +1,14 @@
+// los arreglos son colecciones de valores primitivos, cuya longitud es conocida.
+
+int[] miArray = new int[5];
+miArray[0] = 1;
+
+Console.WriteLine(miArray[0]);
+
+// sintaxis alternativa
+// es posible asignar los valores del arreglo al momento de la declaración.
+int[] miOtroArray = {};
+Console.WriteLine(miOtroArray);
+
+// habrán ocasiones que no vamos a saber de antemano el tamaño de un array, será necesario en algún
+// momento conocer su tamaño.
