b65b51f6874f3f5795b4be88b25173bdafac65f2
Diego Herrera <psystingod@gmail.com>
2023-08-01 10:07:17 -0600

    functions-1 was added

---
+++ functions/functions-1.cs
@@ -0,0 +1,10 @@
+// Las funciones son bloques de código reutilizables.
+// pueden devolver un valor en su invocación.
+
+// declaración de una función.
+
+int sumar() {
+
+}
+
+// la función genera un error. ¿Por qué?
